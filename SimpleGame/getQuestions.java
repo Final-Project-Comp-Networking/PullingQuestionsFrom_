4af83176f62ef26c25dd4df71d0178ad48f6e2f3
BetzKJ <123523850+BetzKJ@users.noreply.github.com>
2024-04-30 14:49:05 -0500

    Update getQuestions.java

--- SimpleGame/getQuestions.java
+++ SimpleGame/getQuestions.java
@@ -1,36 +1,57 @@
-package SimpleGame;
+package com.jdbc.user;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class getQuestions {
-	
-	public static int getRandomQuestion(String[][] questionList) throws Exception { 
-        Random random = new Random();
-        int randNum = random.nextInt(questionList.length);
-        return randNum;
-        //Note: Can currently pull the same # again. Will write code in the future to get around this issue
-    }
-	
-	public static String[][] questionList() throws Exception {
-		File file = new File("\\Users\\kjbet\\Questions.txt"); //Temp filepath 
-		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-			String[][] questions = new String[10][6]; 
-			// Number of columns and rows currently hard coded due to file.length basically being the # of characters in the file
-			//Want to see if there's an easier way, but after spending hours on dealing with randomaccessfile
-			//and having to scrap that, rn here's a simple solution
-			for (int i = 0; i < 10; i++) {
-				String string = reader.readLine(); 
-				String[] arrString = string.split(",", 0);
-				int j = 0;
-					for (String a : arrString) {
-						questions[i][j] = a;
-						j++;
+		
+		public static int getRandomQuestion(String[][] questionList) throws Exception { 
+	        Random random = new Random();
+	        int randNum = random.nextInt(questionList.length);
+	        return randNum;
+	    }
+		
+		public static String[][] questionList() throws Exception {
+			File file = new File("Questions.txt");
+			try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+				String[][] questions = new String[28][6]; //change the first number to amount of questions
+				
+				ArrayList<Integer> list = new ArrayList<Integer>();
+				
+				for (int i = 0; i < questions.length; i++) { //change the amount of questions that will be put into the possible questions and creates random order
+					
+					String string = reader.readLine(); 
+					
+					String[] arrString = string.split(",", 0);
+					
+					int num = getRandomQuestion(questions);
+					
+					while(getFlag(list, num)) {
+						num = getRandomQuestion(questions);
 					}
+					
+					int j = 0;
+					
+						for (String a : arrString) {
+							questions[num][j] = a;
+							j++;
+						}
+						
+					list.add(num);
+				}
+				return questions;
 			}
-			return questions;
+		}
+		
+		public static boolean getFlag(ArrayList<Integer> list, int num) {
+			for(int i = 0; i < list.size(); i++) {
+				if(list.get(i) == num) {
+					return true;
+				}
+			}
+			return false;
 		}
 	}
-}
